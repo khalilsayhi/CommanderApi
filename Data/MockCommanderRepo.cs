97e9571410b10dd5958d824af216e1def34d6106
khalilsayhi <khalil.sayhi1@esprit.tn>
2020-12-05 23:14:41 +0100

    second commit

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command{Id=0,HowTo="Boil an egg", Line="Boil water", Platform="Kettle & pan"},
+                new Command{Id=2,HowTo="do something else", Line="do it", Platform="pan & pen"},
+                new Command{Id=1,HowTo="make tea", Line="Boil water", Platform="teaPot"}
+                
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command{Id=0,HowTo="Boil an egg", Line="Boil water", Platform="Kettle & pen"};
+            
+        }
+    }
+}
